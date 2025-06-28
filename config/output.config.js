522991cb7b59e6f126393b21d04aa1dfaae829ca
Alex Sanudo <asanudolopez+git@gmail.com>
2025-06-28 16:10:09 -0300

    Output to tmp

--- config/output.config.js
+++ config/output.config.js
@@ -2,7 +2,7 @@ const config = {
   outputFileName: 'CanAm_Pricing',
   hrefsFileName: 'fixtures/hrefs.json',
   vehiclesByYearFilename: 'fixtures/vehiclesByYear.json',
-  partsByVehicleFilename: 'fixtures/partsByVehicle.json',
+  partsByVehicleFilename: 'tmp/partsByVehicle.json',
 };
 
 export default config;
