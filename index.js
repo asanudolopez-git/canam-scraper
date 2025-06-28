aaacacbe18eac0aeb85798b4f76a21ffc0b6e618
Alex Sanudo <asanudolopez+git@gmail.com>
2025-06-28 02:07:29 -0300

    All The things have been scraped

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ dotenv.config();
 import withLogin from './withLogin.js';
 import config from './config.js';
 import { getPartsFromVehicleHref } from './navigation.js';
+import { getYearRange } from './utils.js';
 
 const scrapeParts = async () => {
   withLogin(async page => {
-    const years = [2025]; // For testing, you can set this to a specific year or range
+    const years = getYearRange(2000, 2025); // For testing, you can set this to a specific year or range
 
     const vehiclesByYear = JSON.parse(fs.readFileSync(config.vehiclesByYearFilename, 'utf8'));
     const partsByVehicle = JSON.parse(fs.readFileSync(config.partsByVehicleFilename, 'utf8')) || {};
     for (const year of years) {
       const vehicles = vehiclesByYear[year] || [];
-      if (vehicles.length == 0) { return; }
 
       console.log(`Scraping parts for ${vehicles.length} vehicles for year ${year}...`);
       for (const vehicleByYear of vehicles) {
@@ -22,12 +22,12 @@ const scrapeParts = async () => {
         const vehicleParts = partsByVehicle[vehicleId] || [];
         // Tmp: Indicates it has already been scraped. Include boolean here for optional updates
         if (partsByVehicle[vehicleId]) {
-          console.log(`${vehicleParts} has already been scraped, skipping...`)
+          console.log(`${vehicleId} has already been scraped, skipping...`)
           continue;
         }
 
         try {
-          console.log(`Scraping parts for: ${vehicleParts}`);
+          console.log(`Scraping parts for: ${vehicleId}`);
           const parts = await getPartsFromVehicleHref(page, vehicleId);
           console.log(`Found ${parts.length} parts for ${vehicleId}.`);
           vehicleParts.push(...parts.map(
