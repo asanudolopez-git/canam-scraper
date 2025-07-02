c155a216d387d5820d00085344fac045fda46c79
Alex Sanudo <asanudolopez+git@gmail.com>
2025-07-02 16:17:21 -0300

    moved eslint config

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
-
 export default defineConfig([
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
   {
